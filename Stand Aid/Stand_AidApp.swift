2c8446be960f8bf6bd6f132ed6611b87ca07e24d
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-04 01:32:01 -0500

    Initial Commit

---
+++ Stand Aid/Stand_AidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stand_AidApp.swift
+//  Stand Aid
+//
+//  Created by Nicholas Gramenos on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Stand_AidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
